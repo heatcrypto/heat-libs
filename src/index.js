81d0ea170df1106b4ea0f28a9afdede95570f21a
Abhiram Hatolkar <ahatolkar@grepruby.com>
2018-06-29 11:23:23 +0530

    Added bitcore-explorers

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ const bip39           = require('bip39')
 const lightwallet     = require('eth-lightwallet')
 const SolidityCoder   = require('web3/lib/solidity/coder')
 const Web3            = require('web3')
+const explorers       = require('bitcore-explorers')
 
 module.exports = {
   bitcore: bitcore,
   bip39: bip39,
   lightwallet: lightwallet,
   __SolidityCoder: SolidityCoder,
-  Web3: Web3
+  Web3: Web3,
+  explorers: explorers
 }
