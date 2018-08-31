e8fb6c100a71f21a4459b50a2f91f161613dc0bb
Abhiram Hatolkar <ahatolkar@grepruby.com>
2018-08-31 19:21:47 +0530

    Added hooked-web3-provider

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const lightwallet     = require('eth-lightwallet')
 const SolidityCoder   = require('web3/lib/solidity/coder')
 const Web3            = require('web3')
 const explorers       = require('bitcore-explorers')
+const HookedWeb3Provider = require('hooked-web3-provider')
 
 module.exports = {
   bitcore: bitcore,
@@ -11,5 +12,6 @@ module.exports = {
   lightwallet: lightwallet,
   __SolidityCoder: SolidityCoder,
   Web3: Web3,
-  explorers: explorers
+  explorers: explorers,
+  HookedWeb3Provider: HookedWeb3Provider
 }
